5ffbda4ba61fbedbba5ea1c3b5e5eddd0b28b036
Alexande Becker <a.becker@cern.ch>
2023-07-19 16:41:28 +0800

    Worked on the procedure configuration gui

--- ntu_daq_gui/gui.py
+++ ntu_daq_gui/gui.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 import json
+from ntu_daq_gui import procedure as prc
+from functools import partial
+
 
 class GUI(tk.Tk):
     def __init__(self, config_params, config_file_path):
@@ -15,14 +18,20 @@ class GUI(tk.Tk):
 
         # Create the two tabs
         self.run_control_tab = ttk.Frame(self.notebook)
-        self.config_params_tab = ttk.Frame(self.notebook)
+        self.procedure_config_tab = ttk.Frame(self.notebook)
         self.notebook.add(self.run_control_tab, text='Run Control')
-        self.notebook.add(self.config_params_tab, text='Configuration')
+        self.notebook.add(self.procedure_config_tab, text='Configuration')
 
         # Run Control Tab
-        self.procedures = self.config_params.get("procedures", [])
+        self.procedures = list(
+                map(lambda p: prc.Procedure(
+                    p.get("name"),
+                    p.get("executed_file", ""),
+                    p.get("options", {}),
+                    p.get("initial_dut_config", "")),
+                    self.config_params.get("procedures", [])))
         self.procedure_var = tk.StringVar()
-        self.procedure_var.set(self.procedures[0])
+        self.procedure_var.set(self.procedures[0].name)
 
         # Create left and right frames in the run control tab
         left_frame = ttk.Frame(self.run_control_tab)
@@ -35,9 +44,10 @@ class GUI(tk.Tk):
         procedure_label = ttk.Label(left_frame, text="Select Procedure:")
         procedure_label.pack()
 
-        procedure_dropdown = ttk.OptionMenu(left_frame,
-                                            self.procedure_var,
-                                            *self.procedures)
+        procedure_dropdown = ttk.OptionMenu(
+                left_frame,
+                self.procedure_var,
+                *list(map(lambda p: p.name, self.procedures)))
         procedure_dropdown.pack(fill='x')
 
         run_button = ttk.Button(left_frame,
@@ -57,78 +67,49 @@ class GUI(tk.Tk):
         self.log_text.pack(fill='both', expand=True)
 
         # Config Parameters Tab
-        self.config_params.pop("procedures", None)  # Remove "procedures" from config
+        self.config_params.pop("procedures", None)
 
-        self.create_config_widgets()
+        self.setup_procedure_widgets()
 
         # Configure grid weights
         self.run_control_tab.columnconfigure(1, weight=1)
         self.run_control_tab.rowconfigure(0, weight=0)
         self.run_control_tab.rowconfigure(1, weight=1)
 
-
     def save_config(self):
-        with open(CONFIG_FILE, 'w') as f:
+        """
+        Write the config parameters to the config file
+        """
+        with open(self.config_file_path, 'w') as f:
             json.dump(self.config_params, f)
 
-    def create_procedures_widgets(self):
-        procedures_frame = ttk.Frame(self.config_params_tab)
+    def setup_procedure_widgets(self):
+        procedures_frame = ttk.Frame(self.procedure_config_tab)
         procedures_frame.pack()
 
-        add_button = ttk.Button(procedures_frame, text="Add Procedure", command=self.add_procedure)
+        add_button = ttk.Button(procedures_frame,
+                                text="Add Procedure",
+                                command=self.add_procedure)
         add_button.pack()
+        self.create_procedures_widgets(procedures_frame)
 
-        for procedure in self.procedures:
-            procedure_frame = ttk.Frame(procedures_frame)
-            procedure_frame.pack()
-
-            name_label = ttk.Label(procedure_frame, text="Procedure Name:")
-            name_label.pack(side='left')
-
-            name_entry = ttk.Entry(procedure_frame)
-            name_entry.insert(0, procedure.get("name", ""))
-            name_entry.pack(side='left')
-
-            file_label = ttk.Label(procedure_frame, text="Executed File:")
-            file_label.pack(side='left')
-
-            file_entry = ttk.Entry(procedure_frame)
-            file_entry.insert(0, procedure.get("file", ""))
-            file_entry.pack(side='left')
-
-            remove_button = ttk.Button(procedure_frame, text="Remove", command=lambda frame=procedure_frame: self.remove_procedure(frame))
-            remove_button.pack(side='left')
+    def create_procedures_widgets(self, parent):
+        for i, procedure in enumerate(self.procedures):
+            proc_frame = ttk.Frame(parent, borderwidth=2, relief="groove")
+            proc_frame.pack(padx=5, pady=5, fill='x')
+            prc.ProcedureConfigUI(proc_frame, procedure)
+            ttk.Button(proc_frame, text="Remove Procedure",
+                       command=partial(self.remove_procedure, i, parent))
 
     def add_procedure(self):
-        procedure = {"name": "", "file": ""}  # New procedure dictionary
-        self.procedures.append(procedure)
-        self.create_procedures_widgets()
+        pcui = prc.ProcedureCreationUI(self.procedure_config_tab)
+        self.procedures.append(pcui.result)
+        self.create_procedures_widgets(self.procedure_config_tab)
 
-    def remove_procedure(self, frame):
-        frame.pack_forget()  # Remove the procedure frame from the GUI
-        procedure = {"name": "", "file": ""}  # Find the corresponding procedure in the procedures list
-        for index, item in enumerate(self.procedures):
-            if item == procedure:
-                self.procedures.pop(index)
-                break
-
-    def create_config_widgets(self):
-        self.create_procedures_widgets()
-        for param, value in self.config_params.items():
-            label = ttk.Label(self.config_params_tab, text=param)
-            label.pack()
-            entry = ttk.Entry(self.config_params_tab)
-            entry.insert(0, value)
-            entry.pack(fill='x')
-            entry.bind("<FocusOut>", self.update_config_param)
-
-    def update_config_param(self, event):
-        widget = event.widget
-        param = widget.get()
-        label = widget.master.winfo_children()[widget.grid_info()['row'] - 1]
-        param_name = label.cget("text")
-        self.config_params[param_name] = param
-        self.save_config()
+    def remove_procedure(self, proc_idx, parent):
+        parent.pack_forget()  # Remove the procedure frame from the GUI
+        self.procedures.pop(proc_idx)
+        self.create_procedures_widgets(parent)
 
     def run_procedure(self):
         procedure = self.procedure_var.get()
--- ntu_daq_gui/procedure.py
+++ ntu_daq_gui/procedure.py
@@ -1,17 +1,24 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk
 from tkinter import filedialog
 from tkinter import simpledialog
 import json
 import os
+from typing import Tuple, Dict
 
 
-class ProcedureConfiguration:
-    def __init__(self, name, executed_file, options, initial_dut_config):
+class Procedure:
+    def __init__(self, name, executed_file,
+                 options: Dict[str, Tuple[str, str]],
+                 initial_dut_config,
+                 hostname_flag: str = "-i", port_flag: str = "-p"):
         self.name = name
         self.executed_file = executed_file
         self.options = options
         self.initial_dut_config = initial_dut_config
+        self.hostname_flag = hostname_flag
+        self.port_flag = port_flag
+        self.running = False
 
     def serialize(self):
         return json.dumps({
@@ -24,13 +31,130 @@ class ProcedureConfiguration:
     @classmethod
     def deserialize(cls, json_string):
         data = json.loads(json_string)
-        return cls(data['name'], data['executed_file'], data['options'], data['initial_dut_config'])
+        return cls(data['name'],
+                   data['executed_file'],
+                   data['options'],
+                   data['initial_dut_config'])
+
+    def add_option(self, name, flag, value):
+        self.options[name] = (flag, value)
+
+    def get_options(self):
+        return self.options.items()
+
+    def gen_run_command(self, hexctrl_hostname: str, hexctrl_port: str):
+        run_command_args = []
+        run_command_args.append(self.executed_file)
+        run_command_args.append(self.hostname_flag)
+        run_command_args.append(hexctrl_hostname)
+        run_command_args.append(self.port_flag)
+        run_command_args.append(hexctrl_port)
+        run_command_args.append("-f")
+        run_command_args.append(self.initial_dut_config)
+        for option in self.options:
+            run_command_args.append(str(option[0]))
+            run_command_args.append(str(option[1]))
 
 
-class ProcedureConfigEditor:
-    def __init__(self, parent, config):
+class OptionDialog(simpledialog.Dialog):
+    def body(self, master):
+        self.title("Add Option")
+
+        tk.Label(master, text="Name:").grid(row=0, sticky=tk.W)
+        tk.Label(master, text="Flag:").grid(row=1, sticky=tk.W)
+        tk.Label(master, text="Value:").grid(row=2, sticky=tk.W)
+
+        self.name_entry = tk.Entry(master)
+        self.name_entry.grid(row=0, column=1)
+
+        self.flag_entry = tk.Entry(master)
+        self.flag_entry.grid(row=1, column=1)
+
+        self.val_entry = tk.Entry(master)
+        self.val_entry.grid(row=2, column=1)
+
+    def apply(self):
+        name = self.name_entry.get()
+        flag = self.flag_entry.get()
+        value = self.val_entry.get()
+        self.result = (name, flag, value)
+
+
+class ProcedureCreationUI(simpledialog.Dialog):
+    def body(self, master):
+        self.title("Create DataClass")
+        self.__mas = master
+        self.options = {}
+
+        tk.Label(master, text="Name:").grid(row=0, sticky=tk.W)
+        tk.Label(master, text="Script File:").grid(row=1, sticky=tk.W)
+        tk.Label(master, text="Initial Config:").grid(row=2, sticky=tk.W)
+
+        self.name_entry = tk.Entry(master)
+        self.name_entry.grid(row=0, column=1)
+
+        self.exec_file_entry = tk.Entry(master)
+        self.exec_file_entry.grid(row=1, column=1)
+        self.exec_file_button = tk.Button(
+                master,
+                text="Browse",
+                command=self.browse_executed_file).grid(row=1, column=2)
+
+        self.init_conf_entry = tk.Entry(master)
+        self.init_conf_entry.grid(row=2, column=1)
+        self.init_conf_button = tk.Button(
+                master,
+                text="Browse",
+                command=self.browse_init_config).grid(row=2, column=2)
+
+        self.add_option_button = tk.Button(
+                master,
+                text="Add Command Option",
+                command=self.add_option).grid(row=3, column=1)
+
+        tk.Label(master, text="Options")
+        self.tree = ttk.Treeview(master,
+                                 columns=("Description", "flag", "Value"),
+                                 show="headings")
+        self.tree.heading("Description", text="Description")
+        self.tree.heading("flag", text="Cmd Flag")
+        self.tree.heading("Value", text="Value")
+        self.tree.grid(columnspan=3, padx=3)
+        master.columnconfigure(1, weight=1)
+
+    def browse_executed_file(self):
+        file_path = filedialog.askopenfilename(title="Select Executed File")
+        if file_path:
+            self.exec_file_entry.delete(0, tk.END)
+            self.exec_file_entry.insert(tk.END, file_path)
+
+    def browse_init_config(self):
+        file_path = filedialog.askopenfilename(
+                title="Select Initial DUT Config")
+        if file_path:
+            self.init_conf_entry.delete(0, tk.END)
+            self.init_conf_entry.insert(tk.END, file_path)
+
+    def add_option(self):
+        od = OptionDialog(self.__mas)
+        print(od.result)
+        self.options[od.result[0]] = (od.result[1], od.result[2])
+        self.tree.insert("", "end", values=od.result)
+
+    def apply(self):
+        name = self.name_entry.get()
+        script = self.exec_file_entry.get()
+        init_conf = self.init_conf_entry.get()
+        self.result = Procedure(name,
+                                executed_file=script,
+                                initial_dut_config=init_conf,
+                                options=self.options)
+
+
+class ProcedureConfigUI:
+    def __init__(self, parent, procedure: Procedure):
         self.parent = parent
-        self.config = config
+        self.procedure = procedure
 
         self.name_label = tk.Label(parent, text="Name:")
         self.name_label.pack()
@@ -41,15 +165,18 @@ class ProcedureConfigEditor:
         self.executed_file_label.pack()
         self.executed_file_entry = tk.Entry(parent)
         self.executed_file_entry.pack()
-        self.executed_file_button = tk.Button(parent, text="Browse", command=self.browse_executed_file)
+        self.executed_file_button = tk.Button(
+                parent, text="Browse", command=self.browse_executed_file)
         self.executed_file_button.pack()
 
-        self.initial_dut_config_label = tk.Label(parent, text="Initial DUT Config:")
-        self.initial_dut_config_label.pack()
-        self.initial_dut_config_entry = tk.Entry(parent)
-        self.initial_dut_config_entry.pack()
-        self.initial_dut_config_button = tk.Button(parent, text="Browse", command=self.browse_initial_dut_config)
-        self.initial_dut_config_button.pack()
+        self.init_config_label = tk.Label(
+                parent, text="Initial DUT Config:")
+        self.init_config_label.pack()
+        self.init_config_entry = tk.Entry(parent)
+        self.init_config_entry.pack()
+        self.init_config_button = tk.Button(
+                parent, text="Browse", command=self.browse_initial_dut_config)
+        self.init_config_button.pack()
 
         self.options_label = tk.Label(parent, text="Options:")
         self.options_label.pack()
@@ -57,31 +184,25 @@ class ProcedureConfigEditor:
         self.options_frame = tk.Frame(parent)
         self.options_frame.pack()
 
-        self.add_option_button = tk.Button(parent, text="Add Option", command=self.add_option)
+        self.add_option_button = tk.Button(parent,
+                                           text="Add Option",
+                                           command=self.add_option)
         self.add_option_button.pack()
-
-        self.serialize_button = tk.Button(parent, text="Serialize", command=self.serialize)
-        self.serialize_button.pack()
 
         self.load_config()
 
     def load_config(self):
-        self.name_entry.insert(tk.END, self.config.name)
-        self.executed_file_entry.insert(tk.END, self.config.executed_file)
-        self.initial_dut_config_entry.insert(tk.END, self.config.initial_dut_config)
+        self.name_entry.insert(tk.END, self.procedure.name)
+        self.executed_file_entry.insert(tk.END, self.procedure.executed_file)
+        self.init_config_entry.insert(tk.END, self.procedure.initial_dut_config)
 
-        for option_name, (flag, value) in self.config.options.items():
+        for option_name, (flag, value) in self.procedure.options.items():
             self.create_option_entry(option_name, flag, value)
 
     def add_option(self):
-        option_name = simpledialog.askstring("Add Option", "Enter option name:")
-        if option_name:
-            flag = simpledialog.askstring("Add Option", f"Enter command flag for option '{option_name}':")
-            if flag:
-                value = simpledialog.askstring("Add Option", f"Enter value for option '{option_name}':")
-                if value:
-                    self.config.options[option_name] = (flag, value)
-                    self.create_option_entry(option_name, flag, value)
+        od = OptionDialog(self.parent)
+        self.procedure.add_option(*od.result)
+        self.create_option_entry(*od.result)
 
     def create_option_entry(self, option_name, flag, value):
         option_frame = tk.Frame(self.options_frame)
@@ -95,36 +216,22 @@ class ProcedureConfigEditor:
 
         value_label = tk.Label(option_frame, text=f"Value: {value}")
         value_label.pack(side=tk.LEFT)
-        
-        remove_button = tk.Button(option_frame, text="Remove", command=lambda: self.remove_option(option_name))
+
+        remove_button = tk.Button(option_frame, text="Remove",
+                                  command=lambda: self.remove_option(option_name))
         remove_button.pack(side=tk.LEFT, padx=5)
 
     def remove_option(self, option_name):
-        if option_name in self.config.options:
-            del self.config.options[option_name]
+        if option_name in self.procedure.options:
+            del self.procedure.options[option_name]
             self.refresh_option_entries()
 
     def refresh_option_entries(self):
         for child in self.options_frame.winfo_children():
             child.destroy()
 
-        for option_name, (flag, value) in self.config.options.items():
+        for option_name, (flag, value) in self.procedure.options.items():
             self.create_option_entry(option_name, flag, value)
-
-    def serialize(self):
-        self.config.name = self.name_entry.get()
-        self.config.executed_file = self.executed_file_entry.get()
-        self.config.initial_dut_config = self.initial_dut_config_entry.get()
-
-        if not self.check_file_exists(self.config.executed_file):
-            messagebox.showerror("File Not Found", f"The executed file '{self.config.executed_file}' does not exist. Please reenter path")
-            return None
-
-        if not self.check_file_exists(self.config.initial_dut_config):
-            messagebox.showerror("File Not Found", f"The initial module config file '{self.config.initial_dut_config}' does not exist, Please reenter path")
-            return None
-
-        return self.config.serialize()
 
     def check_file_exists(self, filepath):
         return os.path.isfile(filepath)
@@ -132,29 +239,31 @@ class ProcedureConfigEditor:
     def browse_executed_file(self):
         file_path = filedialog.askopenfilename(title="Select Executed File")
         if file_path:
-            self.config.executed_file = file_path
+            self.procedure.executed_file = file_path
             self.executed_file_entry.delete(0, tk.END)
             self.executed_file_entry.insert(tk.END, file_path)
 
     def browse_initial_dut_config(self):
         file_path = filedialog.askopenfilename(title="Select Initial DUT Config")
         if file_path:
-            self.config.initial_dut_config = file_path
-            self.initial_dut_config_entry.delete(0, tk.END)
-            self.initial_dut_config_entry.insert(tk.END, file_path)
+            self.procedure.initial_dut_config = file_path
+            self.init_config_entry.delete(0, tk.END)
+            self.init_config_entry.insert(tk.END, file_path)
+
 
 # Usage example:
-name = "Procedure 1"
-executed_file = "script.py"
-initial_dut_config = "dut_config.json"
-options = {
-    'option1': ('-o1', 'value1'),
-    'option2': ('-o2', 'value2'),
-    'option3': ('-o3', 'value3'),
-}
+if __name__ == "__main__":
+    name = "Procedure 1"
+    executed_file = "script.py"
+    initial_dut_config = "dut_config.json"
+    options = {
+        'option1': ('-o1', 'value1'),
+        'option2': ('-o2', 'value2'),
+        'option3': ('-o3', 'value3'),
+    }
 
-config = ProcedureConfiguration(name, executed_file, options, initial_dut_config)
+    config = Procedure(name, executed_file, options, initial_dut_config)
 
-root = tk.Tk()
-editor = ProcedureConfigEditor(root, config)
-root.mainloop()
+    root = tk.Tk()
+    editor = ProcedureConfigUI(root, config)
+    root.mainloop()
